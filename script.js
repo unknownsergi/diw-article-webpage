180dd9d41b324ec59a4cd335c3c92c5dd8e72e28
flame <sergicampsgili@gmail.com>
2023-11-02 20:28:36 +0100

    Activity_1 githubpages

---
+++ script.js
@@ -0,0 +1,7 @@
+function displayRegisterForm() {
+    document.getElementById("form-wrapper").style.display = "flex";
+}
+
+function cancelRegister() {
+    document.getElementById("form-wrapper").style.display = "none";
+}
